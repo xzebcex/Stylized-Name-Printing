5850890f3e9cb727a9a12e43a260af62f980844d
Aman Pandit <amanpandit3027@gmail.com>
2023-01-29 11:31:44 +0530

    Create stylized.py

---
+++ stylized.py
@@ -0,0 +1,12 @@
+# Stylized Name Printing.
+
+# Prompt the user to enter their name.
+name = input('Enter name: ')
+
+# Print the string "###Hello###".
+print('###Hello###')
+
+# Iterate through each letter in the entered name.
+for i in name:
+    # Print each letter surrounded by asterisks.
+    print('****'+i+'****')
