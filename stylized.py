430e8981c22a0a4a34cde12d0b5fc1a6970492cd
Aman Pandit <amanpandit3027@gmail.com>
2023-02-02 16:25:18 +0530

    Update stylized.py
    Using justifying text center() method.

--- stylized.py
+++ stylized.py
@@ -5,10 +5,11 @@ name = input('Enter name: ')
 
 # Print the string "###Hello###".
 #print('###Hello###')
-print('Hello'.center(10, '#'))
+print('Hello'.center(10, '#')) # using justifying text center() method.
 
 # Iterate through each letter in the entered name.
 for i in name:
     # Print each letter surrounded by asterisks.
     #print('****'+i+'****') 
-    print(i.center(10, '*')) 
+    print(i.center(10, '*')) # using justifying text center() method.
+
